bc4e3997cd28fa0167ab5378eae8e869738e3888
Kainat Nadeem <bc220422631kna@vu.edu.pk>
2026-04-30 16:21:09 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,20 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import { nitro } from 'nitro/vite';
 import viteReact from '@vitejs/plugin-react';
-import path from 'path'; // 1. Import path
+import tailwindcss from '@tailwindcss/vite'; // Import Tailwind v4 plugin
+import tsconfigPaths from 'vite-tsconfig-paths'; // Import path resolver
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     tanstackStart(), 
+    tsconfigPaths(), // Automatically handles the @/ alias from tsconfig
+    tailwindcss(),   // Injects and compiles Tailwind v4 styles
     nitro({ preset: 'vercel' }), 
     viteReact()
   ],
   resolve: {
     alias: {
-      // 2. Define the alias to point to your src directory
       "@": path.resolve(__dirname, "./src"),
     },
   },
