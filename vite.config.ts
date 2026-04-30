d66888dd4f39aa94bd32796ef6156dc37c668186
Kainat Nadeem <bc220422631kna@vu.edu.pk>
2026-04-30 15:55:24 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
-import { nitro } from 'nitro/vite'; // Import Nitro
+import { nitro } from 'nitro/vite';
 import viteReact from '@vitejs/plugin-react';
+import path from 'path'; // 1. Import path
 
 export default defineConfig({
   plugins: [
     tanstackStart(), 
-    nitro({ preset: 'vercel' }), // Force the Vercel preset
+    nitro({ preset: 'vercel' }), 
     viteReact()
   ],
+  resolve: {
+    alias: {
+      // 2. Define the alias to point to your src directory
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
